75b5fb8cd9b64f4cd2f06a93f180edc5c6808d67
Julio Mejia Rojas <julioedgarmejiarojas338@gmail.com>
2017-10-11 13:50:40 -0500

    Add documentation

--- src/pe/cibertec/app/PensionApp.java
+++ src/pe/cibertec/app/PensionApp.java
@@ -6,6 +6,13 @@ import pe.cibertec.pension.Pension;
 
 public class PensionApp {
 
+	/*
+	 * Monto de pension 360
+	Si tienes 1 pagas el 100%
+	Si tienes 2 pagas el 80 % de la pension
+	Si tienes 3 o mas pagas el 70% de la pensión.
+	 * */
+	
 	public static void main(String[] args) {
 		
 		//Sabemos que el monto es 360 , raw data
